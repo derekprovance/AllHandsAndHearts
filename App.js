793c4554224c9548eb6e49dbada5ed48268a35de
Derek Provance <derek@provance.me>
2019-07-10 16:14:37 +0545

    Test aws cognito connect

--- App.js
+++ App.js
@@ -5,7 +5,22 @@ import AppContainer from './containers/App';
 import { AlertProvider } from './containers/AlertContainer';
 import createStore from './redux';
 const { store, persistor } = createStore();
-export default class App extends React.PureComponent {
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+Amplify.configure({
+  Auth: {
+    // REQUIRED - Amazon Cognito Identity Pool ID
+    identityPoolId: 'us-east-2_pEJ5U0LHb',
+    // REQUIRED - Amazon Cognito Region
+    region: 'us-east-2',
+    userPoolId: 'us-east-2_pEJ5U0LHb',
+    userPoolWebClientId: 'b1ue32jicaln89jqv166ne69m'
+  }
+});
+
+class App extends React.PureComponent {
   render() {
     return (
       <Provider store={store}>
@@ -18,3 +33,5 @@ export default class App extends React.PureComponent {
     );
   }
 }
+
+export default withAuthenticator(App);
